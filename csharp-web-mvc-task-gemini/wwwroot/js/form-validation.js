74c119dd14abcc7e556317ca2a3869c8d6f707c5
Milos Kukic <milos042@gmail.com>
2022-06-12 22:09:36 +0200

    Dodata front validacija forme za unos i izmenu radnika

---
+++ csharp-web-mvc-task-gemini/wwwroot/js/form-validation.js
@@ -0,0 +1,114 @@
+﻿
+function formValidation() {
+    var firstName = $('#firstName');
+    var lastName = $('#lastName');
+    var address = $('#address');
+    var netoSalary = $('#netoSalary');
+    var submitBtn = $('#submitBtn');
+
+    var firstNameErr = true;
+    var lastNameErr = true;
+    var addressErr = true;
+    var netoSalaryErr = true;
+
+    if (firstName.val().length == 0) {
+        firstNameErr = true;
+    } else {
+        firstNameErr = false;
+    }
+
+    if (lastName.val().length == 0) {
+        lastNameErr = true;
+    } else {
+        lastNameErr = false;
+    }
+
+    if (address.val().length == 0) {
+        addressErr = true;
+    } else {
+        addressErr = false;
+    }
+
+    if (netoSalary.val().length == 0) {
+        netoSalaryErr = true;
+    } else {
+        netoSalaryErr = false;
+    }
+
+    if (firstNameErr) {
+        firstName.css('border', '2px solid #8e0000');
+    } else {
+        firstName.css('border', '0.5px solid #ced4da');
+    }
+    if (lastNameErr) {
+        lastName.css('border', '2px solid #8e0000');
+    } else {
+        firstName.css('border', '0.5px solid #ced4da');
+    }
+    if (addressErr) {
+        address.css('border', '2px solid #8e0000');
+    } else {
+        firstName.css('border', '0.5px solid #ced4da');
+    }
+    if (netoSalaryErr) {
+        netoSalary.css('border', '2px solid #8e0000');
+    } else {
+        firstName.css('border', '0.5px solid #ced4da');
+    }
+
+    if (!firstNameErr && !lastNameErr && !addressErr && !netoSalaryErr) {
+        return true;
+    } else {
+        return false;
+    }
+}
+$(document).ready(function () {
+    var firstName = $('#firstName');
+    var lastName = $('#lastName');
+    var address = $('#address');
+    var netoSalary = $('#netoSalary');
+    var submitBtn = $('#submitBtn');
+
+    var firstNameErr = true;
+    var lastNameErr = true;
+    var addressErr = true;
+    var netoSalaryErr = true;
+    firstName.on('input', function () {
+        if (firstName.val().length == 0) {
+            firstNameErr = true;
+            firstName.css('border', '2px solid #8e0000');
+        } else {
+            firstNameErr = false;
+            firstName.css('border', '0.5px solid #ced4da');
+        }
+    })
+    lastName.on('input', function () {
+        if (lastName.val().length == 0) {
+            lastNameErr = true;
+            lastName.css('border', '2px solid #8e0000');
+        } else {
+            lastNameErr = false;
+            lastName.css('border', '0.5px solid #ced4da');
+        }
+    })
+    address.on('input', function () {
+        if (address.val().length == 0) {
+            addressErr = true;
+            address.css('border', '2px solid #8e0000');
+        } else {
+            addressErr = false;
+            address.css('border', '0.5px solid #ced4da');
+        }
+    })
+    netoSalary.on('input', function () {
+        if (netoSalary.val().length == 0) {
+            netoSalaryErr = true;
+            netoSalary.css('border', '2px solid #8e0000');
+        } else {
+            netoSalaryErr = false;
+            netoSalary.css('border', '0.5px solid #ced4da');
+        }
+    })
+
+
+})
